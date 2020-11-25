a9fa34f0043db046d8780cdf0437774eb4695eef
KarthikStark9 <74292594+KarthikStark9@users.noreply.github.com>
2020-11-25 12:10:42 +0530

    RenamerXx

--- translation.py
+++ translation.py
@@ -1,6 +1,5 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
-
+    START_TEXT = """ <b>Hi {} , I'm A File Renamer+File To Video Converter Bot With Thumbnail!</b> \n
 <b>Bot Maintained By: @KarthikStark</b> \n
 <b>Do /help for more...</b> \n
 """
@@ -20,18 +19,18 @@ class Translation(object):
     DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b> Reply To A File With /rename fil name extension To rename a file</b>"
-    REPLY_TO_DOC_FOR_C2V = "<b> Reply To An File With /c2v To Convert It Into Streamable video File</b>"
-    REPLY_TO_DOC_FOR_C2A = "<b> Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b> Reply To A File With /rename file name .extension (mkv) To rename a file</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Reply To A File With /c2v To Convert It Into Video </b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Reply To A File/video With /c2a To Convert It Into Audio File</b>"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found,SED🤒"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """Wtf?...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Join @Mai_BoTs If You Found Meh Useful</b>"""
-    HELP_USER = """Ohh You Want Help?? 😅
+    ABOUT_ME = """<b>Meh : \n A Telegram File Renamer Bot Which Can Rename Telegram Files and also Converts Files to Videos.</b>"""
+    HELP_USER = """Want Help??
     
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
-3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
-4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n \n <b>Made With Love By: @KarthikStark</b>"""
+3.🔹 <b>Send A Photo to make it as Thumbnail</b> \n
+4.🔹 <b>I Can Also Convert Files Into Video through /c2v Command </b>"""
 
