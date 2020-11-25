e3a9132a2c4e9345719c6dc5b8383ee3dd89e401
KarthikStark9 <74292594+KarthikStark9@users.noreply.github.com>
2020-11-25 12:16:46 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,29 +1,30 @@
 class Translation(object):
     START_TEXT = """ <b>Hi {} , I'm A File Renamer+File To Video Converter Bot With Thumbnail!</b> \n
 <b>Bot Maintained By: @KarthikStark</b> \n
-<b>Do /help for more...</b> \n
+<b>Send /help for more...</b> \n
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
-    BANNED_USER_TEXT = "Sorry!! But My Owner As Banned You From Using This Service. " 
+    BANNED_USER_TEXT = "Sorry!! But you're not allowed to use this Service. " 
     ABS_TEXT = "What Are You Trying To Do?"
     UPGRADE_TEXT = "CONTACT @KarthikStark "
-    DOWNLOAD_START = "<b>Downloading...</b>"
-    UPLOAD_START = "<b>Uploading...</b>"
+    DOWNLOAD_START = "<b>♻ᗪOᗯNᏞOᗩᗪINᏀ...</b>"
+    UPLOAD_START = "<b>🖨ᑌPᏞOᗩᗪINᏀ...</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Operation Successful**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**🎉OᑭᗴᖇᗩᎢION SᑌᑕᑕᗴSSᖴᑌᏞ..**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @KarthikStark "
     NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @KarthikStark "
-    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ </b> /deletethumbnail ❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared"
+    SAVED_CUSTOM_THUMB_NAIL = "<b> ✔ᎢᕼᑌᗰᗷᑎᗩIᏞ SᗩᐯEᗪ »... 
+</b> /deletethumbnail "
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✖ᎢᕼᑌᗰᗷᑎᗩIᏞ ᑕᏞEᗩᖇEᗪ »..."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully. 😎</b>"
     REPLY_TO_DOC_FOR_RENAME_FILE = "<b> Reply To A File With /rename file name .extension (mkv) To rename a file</b>"
-    REPLY_TO_DOC_FOR_C2V = "<b> Reply To A File With /c2v To Convert It Into Video </b>"
-    REPLY_TO_DOC_FOR_C2A = "<b> Reply To A File/video With /c2a To Convert It Into Audio File</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Reply To A File With /c2v To Convert It Into Video. </b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Reply To A File/video With /c2a To Convert It Into Audio File.</b>"
     CUSTOM_CAPTION_UL_FILE = " "
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found,SED🤒"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """Wtf?...Decrease The Number Of Letters😆😉"""
     ABOUT_ME = """<b>Meh : \n A Telegram File Renamer Bot Which Can Rename Telegram Files and also Converts Files to Videos.</b>"""
@@ -31,6 +32,6 @@ class Translation(object):
     
 1.🔹 <b>Send me any Telegram File.</b> \n
 2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
-3.🔹 <b>Send A Photo to make it as Thumbnail</b> \n
-4.🔹 <b>I Can Also Convert Files Into Video through /c2v Command </b>"""
+3.🔹 <b>Send A Photo to make it as Thumbnail.</b> \n
+4.🔹 <b>I Can Also Convert Files Into Video through /c2v Command. </b>"""
 
